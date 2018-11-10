0df3d7aab754bd331443b8b6e4810e1ce6e33f69
stanislaw-zakrzewski <stanislaw.zakrzewski13@gmail.com>
2018-11-10 10:11:39 +0100

    Added "flickering"

--- WFSGame2018/Assets/Scripts/TitleScreenScripts/TitleFlickeringScript.cs
+++ WFSGame2018/Assets/Scripts/TitleScreenScripts/TitleFlickeringScript.cs
@@ -7,15 +7,30 @@ public class TitleFlickeringScript : MonoBehaviour {
     private Text text;
     private int counter;
     private int change = 1;
+    private Random random;
+    private Vector2 startingPosition;
 
 	// Use this for initialization
 	void Start () {
+        random = new Random();
         text = GetComponent<Text>();
         counter = 0;
+        startingPosition = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if(Random.value > 0.3)
+        {
+            text.transform.position = startingPosition + new Vector2(20 * Random.value, 10 * Random.value);
+        }
+        if(Random.value > 0.8)
+        {
+            text.color = Color.white;
+        } else if(Random.value < 0.2)
+        {
+            text.color = Color.black;
+        }
         counter += change;
         if(counter > 20)
         {
