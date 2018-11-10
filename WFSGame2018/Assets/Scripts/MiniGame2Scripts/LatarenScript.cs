d3a9a2fa44e83e1506e28681502f5a8412cad5e6
MaciejSocha <maciek.socha1@gmail.com>
2018-11-10 18:24:35 +0100

    Za dużo żeby pisać...

--- WFSGame2018/Assets/Scripts/MiniGame2Scripts/LatarenScript.cs
+++ WFSGame2018/Assets/Scripts/MiniGame2Scripts/LatarenScript.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class LatarenScript : MonoBehaviour {
 
-    private Random rng;
+    private Vector3 position;
+    private bool isSwiatlo;
+    public GameObject child;
+    private int cnter;
+       
 
 	// Use this for initialization
 	void Start () {
-        rng = new Random();
-	}
+        position = transform.position;
+        isSwiatlo = false;
+        cnter = (int)(100 * Random.value);           
+        
+    }
 	
+    void Zgas() {
+
+        child.transform.localScale = new Vector3(0, 0, 0);
+    }
+
+    void Zaswiec()
+    {
+        child.transform.localScale = new Vector3(1, 1, 1);
+    }
+
+
 	// Update is called once per frame
 	void Update () {
-		
+        if (cnter % 54 == 0) Zgas();
+        if (cnter % 72 == 0) Zaswiec();
+        cnter++;
+        Debug.Log(cnter);
 	}
 }
