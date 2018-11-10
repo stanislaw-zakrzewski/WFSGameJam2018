c31b5d8b06fec740d497af9094623ad02627ac69
Cyngiel <pstefanski07@gmail.com>
2018-11-10 13:00:28 +0100

    PAUSE lel pauzaaa

---
+++ WFSGame2018/Assets/Scripts/PauseScripts/Pause.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+
+    public static bool gameIsPaused = false;
+    public GameObject pauseUi;
+	
+    void Start()
+    {
+        pauseUi.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            if (gameIsPaused)
+            {
+                resumeGame();
+            }
+            else
+            {
+                pauseGame();
+            }
+        }
+
+	}
+
+    void resumeGame()
+    {
+        pauseUi.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void pauseGame()
+    {
+        pauseUi.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+}
---
+++ WFSGame2018/Assets/Scripts/loadSceneScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadSceneScript : MonoBehaviour {
+
+    public void loadSceneMinigame1()
+    {
+        SceneManager.LoadScene("Pause", LoadSceneMode.Additive);
+        SceneManager.LoadScene("MiniGame1");
+    }
+
+    public void loadSceneMainMenu()
+    {
+        SceneManager.LoadScene("Pause", LoadSceneMode.Additive);
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}
