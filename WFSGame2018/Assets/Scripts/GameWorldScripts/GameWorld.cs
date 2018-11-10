eeee2b42ad8d5088e859c12dfcd53661d6b76e53
Cyngiel <pstefanski07@gmail.com>
2018-11-10 18:54:08 +0100

    no message

---
+++ WFSGame2018/Assets/Scripts/GameWorldScripts/GameWorld.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameWorld : MonoBehaviour {
+
+
+	int score;
+    public Text textScore;
+
+    public void addScore(int dawajCoMasz)
+    {
+        score += dawajCoMasz;
+    }
+
+	void Start () {
+        score = 0;
+        textScore.text = "Score: " + score.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //score += 1;
+        textScore.text = "Score: " + score.ToString();
+    }
+}
