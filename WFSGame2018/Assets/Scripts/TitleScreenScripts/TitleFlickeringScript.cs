bc4463149c980bf9d5454960a76590bb6db7e971
stanislaw-zakrzewski <stanislaw.zakrzewski13@gmail.com>
2018-11-10 09:58:26 +0100

    Some minor changes in title screen

---
+++ WFSGame2018/Assets/Scripts/TitleScreenScripts/TitleFlickeringScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleFlickeringScript : MonoBehaviour {
+    private Text text;
+    private int counter;
+    private int change = 1;
+
+	// Use this for initialization
+	void Start () {
+        text = GetComponent<Text>();
+        counter = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        counter += change;
+        if(counter > 20)
+        {
+            change = -1;
+            text.text = "Pozytywizm";
+        }
+        if(counter < 0)
+        {
+            change = 1;
+            text.text = "Negatywizm";
+        }
+	}
+}
