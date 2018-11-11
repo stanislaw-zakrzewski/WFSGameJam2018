be264b2c00b0ab3f0ad8f45319cf28cb2e660b6a
stanislaw-zakrzewski <stanislaw.zakrzewski13@gmail.com>
2018-11-11 03:19:44 +0100

    Loose in scenes

--- WFSGame2018/Assets/Scripts/Szkapa/PlayerScript.cs
+++ WFSGame2018/Assets/Scripts/Szkapa/PlayerScript.cs
@@ -6,11 +6,14 @@ public class PlayerScript : MonoBehaviour {
 
     public float maxSpeed;
     public float gamespeed;
+    public int lives;
+    public float invincibility;
     // Use this for initialization
     Rigidbody2D body;
 	void Start () {
         body = this.GetComponent<Rigidbody2D>();
-
+        lives = 5;
+        invincibility = 0;
     }
 	
 	// Update is called once per frame
@@ -23,6 +26,16 @@ public class PlayerScript : MonoBehaviour {
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (invincibility <= 0) {
+            lives--;
+            invincibility = 2;
+            if (lives < 0)
+            {
+                //loose
+            }
+        } else
+        {
+            invincibility -= Time.deltaTime;
+        }
     }
 }
